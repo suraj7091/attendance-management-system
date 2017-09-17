7ba63222c2cc38031e440d13e1628dfb400162c6
PAPERPANKS <pnkjgautam05@gmail.com>
2017-09-17 23:59:13 +0530

    redesigning main page

---
+++ hometeacher_new.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+// if(!isset($_SESSION['SESS_FIRST_NAME']) || (trim($_SESSION['SESS_LAST_NAME']) == '')) {
+// 		header("location:login.php");
+//		exit();}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>Attendence Management System</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <!-- <link href="css/mdb.css" rel="stylesheet"> -->
+    <link href="css/main.css" rel="stylesheet">
+    <style rel="stylesheet">
+        body {
+            background-image: url('http://edusources.in/educampus/img/USICT.jpg');
+        } 
+        main {
+            padding-top: 3rem;
+            padding-bottom: 2rem;
+        }
+    </style>
+</head>
+<body>
+        
+    <nav class="navbar navbar-dark">
+        <form class="form-inline">
+            <button class="btn btn-outline-success" type="button">Home</button>
+            
+        </form>
+    </nav>
+    <main>
+        <h2>WELCOME TO ATTENDENCE MANAGEMENT SYSTEM</h2>
+        
+        <p>This portal is based on attendence management system .it  is used for taking attendence digitally and publishing </br>
+        student record to web so that it is available to all .such that it is available to parents so that they can see what their</br>
+        ward are doing during class time. it also helps student in knowing their records for time.
+        </p>
+    </main>
+                
+</body>

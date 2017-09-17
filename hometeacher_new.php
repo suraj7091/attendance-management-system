9d259dfacae930a79f183cdeaae7a8843f0a0037
PAPERPANKS <pnkjgautam05@gmail.com>
2017-09-18 00:26:17 +0530

    links added

--- hometeacher_new.php
+++ hometeacher_new.php
@@ -5,44 +5,107 @@ session_start();
 //		exit();}
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html class="full-height" lang="en"><head>
+<!-- Required meta tags always come first -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>Material Design Bootstrap Template</title>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="css/compile.min.css" rel="stylesheet">
 
-    <title>Attendence Management System</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <!-- Material Design Bootstrap -->
-    <!-- <link href="css/mdb.css" rel="stylesheet"> -->
-    <link href="css/main.css" rel="stylesheet">
-    <style rel="stylesheet">
-        body {
-            background-image: url('http://edusources.in/educampus/img/USICT.jpg');
-        } 
-        main {
-            padding-top: 3rem;
-            padding-bottom: 2rem;
+<style>
+
+    .intro-2 {
+        background: url('http://edusources.in/educampus/img/USICT.jpg') no-repeat center center;
+        background-size: cover;
+    }
+        
+    .navbar {
+        background-color: transparent;
+    }
+    
+    .top-nav-collapse {
+        background-color: #4285F4;
+    }
+    
+    @media only screen and (max-width: 768px) {
+        .navbar {
+            background-color: #4285F4;
         }
-    </style>
-</head>
+    }
+    
+</style>
+<style></style></head>
+
 <body>
-        
-    <nav class="navbar navbar-dark">
-        <form class="form-inline">
-            <button class="btn btn-outline-success" type="button">Home</button>
-            
-        </form>
+
+<!--Main Navigation-->
+<header>
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark indigo scrolling-navbar">
+        <a class="navbar-brand" href="#"><strong>Navbar</strong></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link waves-effect waves-light" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link waves-effect waves-light" href="#">View Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link waves-effect waves-light" href="#">Feed Attendence</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link waves-effect waves-light" href="#">View Class Report</a>
+                </li>
+            </ul>
+        </div>
     </nav>
-    <main>
-        <h2>WELCOME TO ATTENDENCE MANAGEMENT SYSTEM</h2>
-        
-        <p>This portal is based on attendence management system .it  is used for taking attendence digitally and publishing </br>
-        student record to web so that it is available to all .such that it is available to parents so that they can see what their</br>
-        ward are doing during class time. it also helps student in knowing their records for time.
-        </p>
-    </main>
-                
-</body>
+
+    <div class="view intro-2 " style="">
+        <div class="full-bg-img flex-center">
+            <div class="container text-center white-text wow fadeInUp" style="animation-name: none; visibility: visible;">
+                <h2>WELCOME TO ATTENDENCE MANAGEMENT SYSTEM</h2>
+                <br/>
+                <p>This portal is based on attendence management system .it  is used for taking attendence digitally and publishing
+                <br/>
+                student record to web so that it is available to all .</p>          
+                <p>Such that it is available to parents so that they can see what their ward are doing during class time. it also helps student in knowing their records for time</p>
+            </div>
+        </div>
+    </div>
+
+</header>
+<!--Main Navigation-->
+
+<!--Main Layout-->
+<main class="text-center py-5">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Meet The Team</h2>
+            </div>
+        </div>
+    </div>
+
+</main>
+<!--Main Layout-->
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js">
+</script>
+
+
+<script>
+    wow = new WOW().init();
+</script>
+
+<div class="hiddendiv common"></div></body></html>

8fa53bd2ca44abc18138487343a6560245a0dbd6
Rajat Maheshwari (chuckie) <rajatrooney1234@gmail.com>
2017-10-31 19:35:35 +0530

    name updated

--- home.php
+++ home.php
@@ -23,8 +23,8 @@ background-color:#00FFCC;
 
 <body><div style="border:2px solid #009900;width:100%;height:700px;margin-top:0px;padding-top:20px;">
 <div style="background-color:#99FFCC;height:50px;width:600px;margin-left:300px;border:1px solid #000099;border-radius:8px;">
-<button type="button" name="home" class="buttons" style="margin-left:80px;"><a href="home.php"style="text-decoration:none">home</a></button>
-<button type="button" name="viewprofile" class="buttons" ><a href="viewprofile.php" style="text-decoration:none">view profile</a></button>
+<button type="button" name="Home" class="buttons" style="margin-left:80px;"><a href="home.php"style="text-decoration:none">home</a></button>
+<button type="button" name="View_cool_profile" class="buttons" ><a href="viewprofile.php" style="text-decoration:none">view profile</a></button>
 <button type="button" name="viewattendence" class="buttons"><a href="viewattendence.php" style="text-decoration:none">view attendence</a></button>
 <button type="button" name="viewclaassrecord" class="buttons"><a href="viewclassrecord.php" style="text-decoration:none">view class record</a></button>
 <button type="button" name="logout" class="buttons"><a href="logout.php" style="text-decoration:none">logout</a></button></div>
